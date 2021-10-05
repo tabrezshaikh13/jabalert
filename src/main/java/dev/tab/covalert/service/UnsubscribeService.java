7078f9b225f00897794a53952d2f8e8caa333265
Tabrez Shaikh <tabrezshaikh17970@gmail.com>
2021-10-05 19:24:54 +0530

    final commit.

--- src/main/java/dev/tab/covalert/service/UnsubscribeService.java
+++ src/main/java/dev/tab/covalert/service/UnsubscribeService.java
@@ -1,14 +1,17 @@
 package dev.tab.covalert.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
 import dev.tab.covalert.model.Client;
 import dev.tab.covalert.repository.ClientRepository;
 
 @Service
 public class UnsubscribeService {
     
+    @Autowired
+    private EmailService mailService;
     @Autowired
     private ClientRepository clientRepo;
 
@@ -17,9 +20,20 @@ public class UnsubscribeService {
         Client client = clientRepo.findByEmail(email);
         if(client != null) {
             clientExists = true;
+            unsubscribeNotification(email);
             clientRepo.deleteByEmail(email);
         }
         return clientExists;
+    }
+
+    @Async
+    public void unsubscribeNotification(String email) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom("jabalert.com@gmail.com");
+        mailMessage.setSubject("Unsubscribe Alert");
+        mailMessage.setText(email + " just unsubscribed from JabAlert.");
+        mailMessage.setTo("jabalert.com@gmail.com");
+        mailService.sendNotification(mailMessage);
     }
 
 }
